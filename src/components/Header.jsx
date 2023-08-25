cf59b145f4371e216c4a74b3ba7c23e3fa5531d3
afifi brahim <afifi71brahim@gmail.com>
2023-08-25 22:12:24 +0100

    add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,34 @@
 import React from 'react'
-import { Box ,Grid,Typography,Button} from '@mui/material'
 import mobileimg from '../assets/mobile/image-hero@2x.jpg'
+import tabletimg from '../assets/tablet/image-hero.jpg'
+import desktopimg from '../assets/desktop/image-hero@2x.jpg'
 import arrowleft from '../assets/icon-arrow-left.svg'
 const Header = () => {
   return (
-    <Box minHeight={'100vh'}  >
-       <Grid container spacing={2}>
-             <Grid item>
-                 <img width={'100%'} src={mobileimg} alt="" />
-            </Grid> 
-            <Grid p={6} display={'flex'} gap={2} flexDirection={'column'}>
-               <Typography fontSize={60} fontFamily={'Big Shoulders Display'} fontWeight={'bold'} variant='h1'>MODERN <br /> ART GALLERY</Typography>
-               <Typography sx={{color:'#444444', lineHeight:'25px', fontWeight:'lighter'}} variant='p'>The arts in the collection of the Modern Art Gallery all stated from a spark of inspiration. Will pieces inspire you? Visit us and find out.</Typography>
-               <Box sx={{display:'flex', bgcolor:'black', color:'white'}}>
-                  <Button sx={{}}>OUR LOCATION</Button>
-                   <img sty src={arrowleft} alt="" />
-               </Box>
-            </Grid>
-       </Grid>
+    <div className='min-h-screen '>
+       <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-7'>
+             <div className='bg-black hidden xl:flex xl:col-span-2 relative'>
+                 <h1 className='text-xl leading-xl absolute top-[38%] -right-[45%] -translate-y-1/2  font-[800] font-headings  bg-text-gradient text-transparent bg-clip-text'> <span className=''>MODERN</span> <br /> <span className=''>ART GALLERY</span> </h1>
 
-    </Box>
+             </div>
+             <div className=' xl:xl:col-span-3 '>
+                 <img className='md:hidden' src={mobileimg} alt="image.png" />
+                 <img className=' hidden md:flex xl:hidden ' src={tabletimg} alt="image.png" />
+                 <img className=' hidden xl:flex w-full  ' src={desktopimg} alt="image.png" />
+            </div> 
+            <div className='px-4 py-10 flex flex-col gap-6 md:gap-16 md:-ml-20 xl:xl:col-span-2 xl:justify-center  ' >
+               <h1 className='text-m md:text-l leading-m font-[800] font-headings xl:hidden'>MODERN <br /> ART GALLERY</h1>
+               <p className='text-gray leading-body-s text-body-s font-[300] xl:max-w-xs xl:text-body-m'>The arts in the collection of the Modern Art Gallery all stated from a spark of inspiration. Will pieces inspire you? Visit us and find out.</p>
+               <div className='flex  text-white group w-fit duration-200 '>
+                  <button className='h-16 px-6 tracking-[3px] bg-black group-hover:bg-gold duration-200 font-headings text-body-m font-bold'>OUR LOCATION</button>
+                  <div className='bg-gold h-16 cursor-pointer w-14 flex items-center justify-center px-4 group-hover:bg-black duration-200'>
+                      <img className='rotate-180  ' src={arrowleft} alt="" />
+                  </div>
+               </div>
+            </div>
+       </div>
+
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,47 @@
+
 /** @type {import('tailwindcss').Config} */
 export default {
+
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+      ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gold: '#D5966C',
+        black: '#151515',
+        gray: '#444444'
+      }
+    },
+    backgroundImage: {
+      map: 'url("/mobile/image-map@2x.png")',
+      'text-gradient':'linear-gradient(to right, #fff 61%, #151515 39%)'
+    },
+    fontSize: {
+      xl: '6rem', // 96px
+      l: '4.375rem', // 70px
+      m: '3.75rem', // 60px
+      s: '2.25rem', // 36px
+      'body-m': '1.375rem', // 22px
+      'body-s': '1.125rem' // 18px
+    },
+    lineHeight: {
+      xl: '5.5rem', // 88px
+      l: '4.375rem', // 70px
+      m: '3.75rem', // 60px
+      s: '2.25rem', // 36px
+      'body-m': '2rem', // 32px
+      'body-s': '1.75rem' // 28px
+    },
+    fontFamily: {
+      sans: ['Outfit', 'sans-serif'],
+      headings: ['Big Shoulders Display', 'sans-serif']
+    },
+    screens: {
+      md: '750px',
+      xl: '1110px'
+    }
   },
-  plugins: [],
+  plugins: []
 }
